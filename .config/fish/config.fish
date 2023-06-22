a107f4517aec5a745e2ed11ae936af52c84df5e3
Carlos Castillo <47466248+regexPattern@users.noreply.github.com>
2023-06-22 09:50:54 -0300

    removing home manager config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,60 +1,48 @@
-# Disable fish greeting message when new shell starts.
+# disable fish greeting message when new shell starts.
 set fish_greeting
 
-# Editor
-set -g --export EDITOR "nvim --cmd 'lua vim.g.minimal = true'"
-set -g --export FCEDIT "$EDITOR"
-set -g --export GIT_EDITOR "$EDITOR"
-set -g --export VISUAL "$EDITOR"
+# editor
+set -gx EDITOR "nvim --cmd 'lua vim.g.minimal = true'"
+set -gx FCEDIT "$EDITOR"
+set -gx GIT_EDITOR "$EDITOR"
+set -gx VISUAL "$EDITOR"
 
-# Development-related
-set -g --export CARGO_HOME "$HOME/.cargo"
-set -g --export GIT_PAGER "cat"
-set -g --export GOPATH "$HOME/go"
-set -g --export PNPM_HOME "$HOME/.pnpm"
+# development-related
+set -gx CARGO_HOME "$HOME/.cargo"
+set -gx GIT_PAGER "cat"
+set -gx GOPATH "$HOME/go"
+set -gx PNPM_HOME "$HOME/.pnpm"
 
 # bat
-set -g --export BAT_THEME "Nord"
+set -gx BAT_THEME "Nord"
 
 # fzf
-set -g --export FZF_DEFAULT_COMMAND "fd --type f --hidden"
-# set -g --export FZF_DEFAULT_COMMAND "--color=dark"
-
-# Nord
-set -g --export FZF_DEFAULT_OPTS "\
---color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
---color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B"
-
-# Catppuccin
-# set -g --export FZF_DEFAULT_OPTS "\
-# --color=bg+:#313244,bg:#1E1E2E,spinner:#F5E0DC,hl:#F38BA8 \
-# --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC \
-# --color=marker:#F5E0DC,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8"
+set -gx FZF_DEFAULT_COMMAND "fd --type f --hidden"
+set -gx FZF_DEFAULT_OPTS "--color=dark"
+# set -gx FZF_DEFAULT_OPTS "\
+# --color fg:#D8DEE9,bg:#2E3440,hl:#A3BE8C,fg+:#D8DEE9,bg+:#434C5E,hl+:#A3BE8C
+# --color pointer:#BF616A,info:#4C566A,spinner:#4C566A,header:#4C566A,prompt:#81A1C1,marker:#EBCB8B"
 
 # pfetch
-set -g --export PF_INFO "ascii title os host kernel uptime memory"
+set -gx PF_INFO "ascii title os host kernel uptime memory"
 
-# ripgrep
-set -g --export RIPGREP_CONFIG_PATH "$HOME/.config/ripgrep/.ripgreprc"
+# append to path
+fish_add_path "$CARGO_HOME"
+fish_add_path "$GOPATH/bin"
+fish_add_path "$HOME/.flutter/bin"
+fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/.zvm/bin"
+fish_add_path "$PNPM_HOME"
 
-# $PATH
-fish_add_path -m "$CARGO_HOME"
-fish_add_path -m "$GOPATH/bin"
-fish_add_path -m "$HOME/.flutter/bin"
-fish_add_path -m "$HOME/.local/bin"
-fish_add_path -m "$HOME/.zvm/bin"
-fish_add_path -m "$PNPM_HOME"
-
-# Aliases
+# aliases
 alias .. "cd .."
-alias cat "bat --italic-text=always --style=plain --tabs=4"
-alias ll "exa -la --group-directories-first"
+alias cat "bat --style=plain --tabs=4"
+alias ls "exa -la --group-directories-first"
 alias ls "exa -a --group-directories-first"
 
-# Bindings
+# bindings
 bind ! __history_previous_command
 bind '$' __history_previous_command_arguments
 
-# Init
-# Programs that run on shell startup.
+# programs that run on shell startup.
 zoxide init fish | source
--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
-all:
-	stow . --target=${HOME}
+LOG_LEVEL = 1
+XDG_CONFIG_HOME ?= $$HOME/.config
+
+stow:
+	stow -t $(XDG_CONFIG_HOME) .config -v $(LOG_LEVEL)
+
+clean:
+	stow -t $(XDG_CONFIG_HOME) -D .config -v $(LOG_LEVEL)
